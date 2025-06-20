9cee167ebc271ef7ceeedff73e2b305fe8760e89
Giraldo Pino <giraldopino.blinq@gmail.com>
2025-06-20 16:41:00 -0400

    added all parameters

--- nodes/RunAgent/RunAgent.node.ts
+++ nodes/RunAgent/RunAgent.node.ts
@@ -38,17 +38,61 @@ export class RunAgent implements INodeType {
 				type: 'string',
 				default: '',
 				required: true,
-				description: 'The name of the agent or workflow to run. No API infrastructure exists to list agents or workflows at this time.',
+				description: 'The name of the workflow to execute.',
 				routing: {
 					request: {
 						method: 'POST',
-						url: '/workflows/{{$agentName}}/run',
+						url: '/workflows/{{$agentName}}/run'
 					}
 				}
 			},
+			{
+				displayName: 'Workflow Version Number',
+				name: 'workflowVersionNumber',
+				type: 'string',
+				default: '',
+				placeholder: '1',
+				description: 'Specify a workflow version number to run a specific version.',
+			},
+			{
+				displayName: 'Input Variables',
+				name: 'inputVariables',
+				type: 'json',
+				default: '{}',
+				description: 'A dictionary of input variables required by the workflow.',
+			},
 
 			// Optionally fields will go here
-		
+			{
+				displayName: 'Additional Fields',
+				name: 'additionalFields',
+				type: 'collection',
+				default: {},
+				placeholder: 'Add Field',
+				options: [
+					{
+						displayName: 'Metadata',
+						name: 'metadata',
+						type: 'json',
+						default: '{}',
+						description: 'A JSON of metadata key-value pairs.',
+					},
+					{
+						displayName: 'Return All Outputs',
+						name: 'returnAllOutputs',
+						type: 'boolean',
+						default: false,
+						description: 'If set to true, all outputs from the workflow execution will be returned.',
+					},
+					{
+						displayName: 'Workflow Label Name',
+						name: 'workflowLabelName',
+						type: 'string',
+						default: "",
+						description: 'Specify a workflow label name to run a specific labeled version.',
+					}
+				]
+			}	
 		]
 	};
 }
